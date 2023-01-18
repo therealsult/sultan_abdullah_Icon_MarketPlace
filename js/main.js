07e77b7357f1f666f07e183f6a49393915bc5bb7
therealsult <113542978+therealsult@users.noreply.github.com>
2023-01-18 13:54:15 -0500

    second commit

---
+++ js/main.js
@@ -0,0 +1,122 @@
+console.log("JavaScript is running!");
+
+
+/* chart */
+
+
+const svgGraphic = document.querySelector("#chart");
+
+function logID() {
+    console.log(this.id);
+}
+
+svgGraphic.addEventListener("click", logID);
+
+
+
+/* disc */
+
+
+
+const discs = document.querySelector("#discs");
+
+function logID() {
+    console.log(this.id);
+}
+
+discs.addEventListener("click", logID);
+
+
+/* harddrive*/
+
+
+const harddrive = document.querySelector("#harddrive");
+
+function logID() {
+    console.log(this.id);
+}
+
+harddrive.addEventListener("click", logID);
+
+
+/* matrix */
+
+const matrix = document.querySelector("#matrix");
+
+function logID() {
+    console.log(this.id);
+}
+
+matrix.addEventListener("click", logID);
+
+
+/* message */
+
+
+const message = document.querySelector("#message");
+
+function logID() {
+    console.log(this.id);
+}
+
+message.addEventListener("click", logID);
+
+
+/* nike */
+
+
+const nike = document.querySelector("#nike");
+
+function logID() {
+    console.log(this.id);
+}
+
+nike.addEventListener("click", logID);
+
+
+/* square */
+
+const square = document.querySelector("#square");
+
+function logID() {
+    console.log(this.id);
+}
+
+square.addEventListener("click", logID);
+
+
+
+/* triangle */
+
+const triangle = document.querySelector("#triangle");
+
+function logID() {
+    console.log(this.id);
+}
+
+triangle.addEventListener("click", logID);
+
+
+
+/* x */
+
+const x = document.querySelector("#x");
+
+function logID() {
+    console.log(this.id);
+}
+
+x.addEventListener("click", logID);
+
+
+
+/* circle */
+
+
+const circle = document.querySelector("#circle");
+
+function logID() {
+    console.log(this.id);
+}
+
+circle.addEventListener("click", logID);
